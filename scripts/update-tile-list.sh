a0bd0afaf347ba1437a43e63f09a0fc46fffa0cc
Hsieh Chin Fan <typebrook@gmail.com>
2021-01-31 22:24:18 +0800

    Update script for tiles.list
    Sort tiles with newer timestamp

--- scripts/update-tile-list.sh
+++ scripts/update-tile-list.sh
@@ -3,13 +3,16 @@
 LIST=tiles.list && touch $LIST
 LAST_MONTH_TIMESTAMP=$(date --date='-1 month' +%s)
 
+# Put updated tiles into tiles.list
+# First column is current timestamp, second column is tile path
 git status tiles/ --short | awk -v date="$(date +%s)" '{print date, $2}' \
 | cat - $LIST \
 | while read timestamp tile; do
-  # remove entries that over a month ago
+  # Remove entries that over a month ago
   [[ $timestamp -lt $LAST_MOTH_TIMESTAMP ]] && break
   echo $timestamp $tile
 done >$LIST.bak
 
-sort -u -k2 <$LIST.bak >$LIST
+# Remove duplicate tiles (tiles also updated before) and sort them with newer timestamp
+sort -u -k2 <$LIST.bak | sort --reverse >$LIST
 rm $LIST.bak
