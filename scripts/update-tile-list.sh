15653a28cef1109f54926ed5e3da19a9d75b4567
Hsieh Chin Fan <typebrook@gmail.com>
2021-01-31 12:39:05 +0800

    Add restrict to tiles.list
    If entries are over a month ago, remove them from tiles.list

--- scripts/update-tile-list.sh
+++ scripts/update-tile-list.sh
@@ -1,8 +1,15 @@
 #! /bin/bash
 
 LIST=tiles.list && touch $LIST
+LAST_MONTH_TIMESTAMP=$(date --date='-1 month' +%s)
 
-git status tiles/ --short | awk -v date="$(date +%s)" '{print date, $2}' | cat - $LIST >$LIST.bak
+git status tiles/ --short | awk -v date="$(date +%s)" '{print date, $2}' \
+| cat - $LIST \
+| while read timestamp tile; do
+  # remove entries that over a month ago
+  [[ $timestamp -lt $LAST_MOTH_TIMESTAMP ]] && break
+  echo $timestamp $tile
+done >$LIST.bak
 
 sort -u -k2 <$LIST.bak >$LIST
 rm $LIST.bak
